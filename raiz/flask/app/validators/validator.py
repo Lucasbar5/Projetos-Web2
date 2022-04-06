e24f9734e56220f4b18d30db30bb7a20c494e124
Lucasbar5 <88511757+Lucasbar5@users.noreply.github.com>
2022-04-06 10:28:29 -0300

    Delete raiz/flask directory

--- raiz/flask/app/validators/validator.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Optional
-from pydantic import BaseModel, ValidationError
-
-class Aluno_validation(BaseModel):
-    id: int
-    nome: str
-    matricula: int
-    semestre: int
